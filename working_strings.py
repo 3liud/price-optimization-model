712338998f3b5f9a3042a7e0dcf697829b78b5a4
3liud <ssocratos@gmail.com>
2022-09-06 11:04:14 +0300

    simple string methods

---
+++ working_strings.py
@@ -0,0 +1,18 @@
+# strings 
+first_name = "John"
+last_name = "Gibson"
+full_name = first_name + " " + last_name
+
+print( f"hello {full_name} you have been accepted into the program")
+
+flag = False
+if not flag:
+    print("flag is False")
+    
+
+word = "baseball"
+if "b" in word:
+    print(f"{word} contains the character b")
+
+
+print(f"{word} starts with a {(word[0])}")
